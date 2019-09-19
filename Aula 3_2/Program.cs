795dda5b9e0dc4a403a6441875e2461c1554ff2f
FIC <FIC@info.local>
2019-09-19 12:23:17 -0300

    Atividade

---
+++ Aula 3_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double acumuladora =0;
+           int cont = 1;
+           int n;
+
+           do{
+               Console.WriteLine("Digite um número");
+                n=int.Parse(Console.ReadLine());
+               
+               if (n %3 == 0){
+                   acumuladora+=n;
+                   
+                }
+                cont++;
+
+           }while(cont<=20);
+
+           Console.WriteLine(acumuladora);
+        }
+    }
+}
